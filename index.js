947128cff9784ed04e4ce083a1bdcafb4bade4bb
Rui Richard Blaese <ruiblaese@gmail.com>
2019-08-11 19:15:32 -0300

    iniciando modulo 2 (react native)

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import './src/config/ReactotronConfig';
+import './src/config/DevToolsConfig';
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
